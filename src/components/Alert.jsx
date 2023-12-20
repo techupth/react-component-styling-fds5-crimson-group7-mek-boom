cc075396d505011a800946edda05cd5814365514
Nitisit Vithunat <nitisitvithunat@MacBook-Pro-khxng-Nitisit.local>
2023-12-20 22:55:18 +0700

    submit exercise

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,54 +1,114 @@
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
-
-import ErrorIcon from "../assets/icons/frown.jsx";
-import WarningIcon from "../assets/icons/alert-triangle.jsx";
-import InfoIcon from "../assets/icons/alert-circle.jsx";
-import SuccessIcon from "../assets/icons/check-circle.jsx";
+import alerttriangle from "../picture/alerttriangle.svg";
+import alertcircle from "../picture/alertcircle.svg";
+import alertcheckcircle from "../picture/checkcircle.svg";
 
 function Alert(props) {
-  let bgColor = "#F9EBC8";
-  let AlertIcon = InfoIcon;
-
-  if (props.severity === "error") {
-    bgColor = "#F9C8C8";
-    AlertIcon = ErrorIcon;
-  } else if (props.severity === "warning") {
-    bgColor = "#F9D9C8";
-    AlertIcon = WarningIcon;
-  } else if (props.severity === "success") {
-    bgColor = "#CEF7CD";
-    AlertIcon = SuccessIcon;
-  }
-
-  return (
-    <div
-      css={css`
-        margin: 10px;
-        display: flex;
-        align-items: center;
-        justify-content: start;
-        padding-left: 20px;
-        width: 650px;
-        height: 76px;
-        font-family: "Kanit", sans-serif;
-        font-weight: 700;
-        font-size: 20px;
-        color: #444444;
-        border-radius: 10px;
-        background-color: ${bgColor};
-      `}
-    >
+  if (props.color === "red") {
+    return (
       <div
         css={css`
-          margin-right: 20px;
+        padding: 32px;
+        background-color: red;
+        font-size: 24px;
+        border: none;
+        border-radius; 5px;
+        textAlign: center;
+        color:white
         `}
       >
-        <AlertIcon />
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+        >
+          <path
+            d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z"
+            stroke="#ED5050"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+          <path
+            d="M16 16C16 16 14.5 14 12 14C9.5 14 8 16 8 16"
+            stroke="#ED5050"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+          <path
+            d="M9 9H9.01"
+            stroke="#ED5050"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+          <path
+            d="M15 9H15.01"
+            stroke="#ED5050"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+        </svg>
+        <span>{props.text}</span>"
       </div>
-      {props.children}
-    </div>
-  );
+    );
+  } else if (props.color === "orange") {
+    return (
+      <div
+        css={css`
+            padding: 32px;
+            background-color: orange;
+            font-size: 24px;
+            border: none;
+            border-radius; 5px;
+            textAlign: center;
+            color:white
+            `}
+      >
+        <h3>{props.text}</h3>
+        <img src={alerttriangle} />
+      </div>
+    );
+  } else if (props.color === "yellow") {
+    return (
+      <div
+        css={css`
+            padding: 32px;
+            background-color: yellow;
+            font-size: 24px;
+            border: none;
+            border-radius; 5px;
+            textAlign: center;
+            color:white
+            `}
+      >
+        <h3>{props.text}</h3>
+        <img src={alertcircle} />
+      </div>
+    );
+  }
+  {
+    return (
+      <div
+        css={css`
+        padding: 32px;
+        background-color: green;
+        font-size: 24px;
+        border: none;
+        border-radius; 5px;
+        textAlign: center;
+        color:white
+        `}
+      >
+        <h3>{props.text}</h3>
+        <img src={alertcheckcircle} />
+      </div>
+    );
+  }
 }
-
 export default Alert;
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,3 +1,41 @@
 // Start coding here
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
 
-function Button() {}
+function Button(props) {
+  if (props.color === "primary") {
+    return (
+      <div
+        css={css`
+        padding: 32px;
+        background-color: #4CAF50;
+        font-size: 24px;
+        border: none;
+        border-radius; 5px;
+        textAlign: center;
+        color:white
+        `}
+      >
+        <h3>{props.text}</h3>
+      </div>
+    );
+  } else {
+    props.color === "secondary";
+    return (
+      <div
+        css={css`
+          padding: 32px;
+          background-color: pink;
+          font-size: 24px;
+          border: none;
+          border-radius; 5px;
+          textAlign: center;
+          color:white
+          `}
+      >
+        <h3>{props.text}</h3>
+      </div>
+    );
+  }
+}
+export default Button;
